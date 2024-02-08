f65a2fa7423eb64ceb250041d4a917e4cf6fd751
eleanordm <eleanordm83@gmail.com>
2024-02-08 10:14:44 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav-bar></nav-bar>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0px
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+<nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+</nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #425ab9;
+        }
+    }
+}
+</style>
