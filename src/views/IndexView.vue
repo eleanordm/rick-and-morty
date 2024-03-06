ad899415ab86c4db84b7bf4fce58a12d774bff57
eleanordm <eleanordm83@gmail.com>
2024-03-06 17:12:45 +0000

    Comment added

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -109,16 +109,18 @@ export default {
         }
     },
     created: function () {
-        /* Gets character data on page load */
+        /* Short timeout added to show skeleton loading */
         setTimeout(() => {
             this.loading = false
         }, 300)
-    
+
+        /* Gets character data on page load */
         this.getCharacterData()
     },
     methods: {
         /* Gets all character data */
         getCharacterData: async function () {
+            /* 38 characters in total */
             var rawData = await axios.get("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,40,41,42,43,44,45")
             this.characters = rawData.data
         },
