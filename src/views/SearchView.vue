26b2d271123846dd631ca93b89876f2293697f32
eleanordm <eleanordm83@gmail.com>
2024-04-17 11:03:27 +0100

    Search bar added

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,13 @@
 <template>
-<div>
+<div class="flex justify-center">
+    <div class="flex justify-center mt-32 max-w-full items-center bg-blue h-[40px] w-[300px] rounded-full pl-[10px]">
+        <input type="text" placeholder="Name" v-model="id" class="placeholder-white text-white outline-none bg-blue font-sans">
+        <div class="cursor-pointer h-[30px] w-[30px] border-white border-2 rounded-full">
+            <span class="material-symbols-outlined mt-[3px] ml-[3px] text-[20px]">
+                search
+            </span>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -13,3 +21,7 @@ export default {
     }
 }
 </script>
+
+<style>
+
+</style>
