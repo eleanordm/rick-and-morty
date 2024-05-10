5dbf7e7f43e013c7ac64685a7d53e8677021f634
eleanordm <eleanordm83@gmail.com>
2024-05-10 11:12:52 +0100

    About page mobile view complete

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="lg:mt-36 flex justify-left md:justify-center p-10">
-    <div class="w-[1000px] text-center sm:text-left">
+<div class="lg:mt-36 flex justify-left md:justify-center p-10 sm:mt-10">
+    <div class="w-[1000px] text-center md:text-left">
         <div class="font-zen text-blue text-xs md:text-sm">
             Hi there, my name is
         </div>
@@ -13,14 +13,14 @@
         <div class="font-zen text-beige text-sm md:text-md mt-8 md:w-[600px]">
             Specialising in reactive frameworks like Vue.js, reinforced by expertise in HTML, CSS, JavaScript, and Tailwind.
         </div>
-        <div class="mt-8 flex flex-col md:flex-row w-full">
-            <a href="https://www.linkedin.com/in/eleanor-davis-mullen-66596519a/" target="_blank" class="button md:h-[35px] h-[40px] mr-4 text-sm w-full sm:w-[120px] mb-3">
+        <div class="mt-8 flex flex-col md:flex-row w-full items-center">
+            <a href="https://www.linkedin.com/in/eleanor-davis-mullen-66596519a/" target="_blank" class="button text-sm md:mb-0 mb-3 mr-4 md:h-[35px] h-[40px] w-full max-w-xl">
                 LinkedIn
             </a>
-            <a href="mailto:eleanordm83@gmail.com" class="button md:h-[35px] h-[40px] mr-4 text-sm w-full sm:w-[120px] mb-3">
+            <a href="mailto:eleanordm83@gmail.com" class="button text-sm md:mb-0 mb-3 mr-4 md:h-[35px] h-[40px] w-full max-w-xl">
                 Email
             </a>
-            <a href="https://github.com/eleanordm/rick-and-morty" target="_blank" class="button md:h-[35px] h-[40px] text-sm w-full sm:w-[120px]">
+            <a href="https://github.com/eleanordm/rick-and-morty" target="_blank" class="button text-sm  mr-4 md:mb-0 md:h-[35px] h-[40px] w-full max-w-xl">
                 Github
             </a>
         </div>
