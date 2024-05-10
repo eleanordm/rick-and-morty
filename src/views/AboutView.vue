ad252d9de494a9a6f847a51f1f0d71a5de3f6b5c
eleanordm <eleanordm83@gmail.com>
2024-05-10 11:41:44 +0100

    All resolutions complete - about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="lg:mt-36 flex justify-left md:justify-center p-10 sm:mt-10">
+<div class="md:p-[85px] lg:mt-[75px] xl:p-24 flex justify-left md:justify-center p-10 sm:mt-10">
     <div class="w-[1000px] text-center md:text-left">
         <div class="font-zen text-blue text-xs md:text-sm">
             Hi there, my name is
@@ -7,13 +7,13 @@
         <div class="font-zen text-pink text-3xl md:text-6xl mt-5">
             Eleanor Davis Mullen
         </div>
-        <div class="font-zen text-[#f9bfdf] text-1xl md:text-4xl mt-2 opacity-90">
+        <div class="font-zen text-[#f9bfdf] text-1xl md:text-3xl mt-2 opacity-90">
             Junior Software Developer
         </div>
         <div class="font-zen text-beige text-sm md:text-md mt-8 md:w-[600px]">
             Specialising in reactive frameworks like Vue.js, reinforced by expertise in HTML, CSS, JavaScript, and Tailwind.
         </div>
-        <div class="mt-8 flex flex-col md:flex-row w-full items-center">
+        <div class="mt-8 flex flex-col md:flex-row w-full items-center md:max-w-md">
             <a href="https://www.linkedin.com/in/eleanor-davis-mullen-66596519a/" target="_blank" class="button text-sm md:mb-0 mb-3 mr-4 md:h-[35px] h-[40px] w-full max-w-xl">
                 LinkedIn
             </a>
