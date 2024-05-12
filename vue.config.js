0052c02d1ce50b4a7fad5d6234005647650990b3
eleanordm <eleanordm83@gmail.com>
2024-05-12 22:15:31 +0100

    Changed dist to docs in outputDir

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: "dist",
-  publicPath: process.env.NODE_ENV === "production" ? "/rick-and-morty/" : "/about",
+  outputDir: "docs",
+  publicPath: process.env.NODE_ENV === "production" ? "/rick-and-morty/" : "/",
 })
