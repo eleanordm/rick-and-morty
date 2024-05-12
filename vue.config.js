10a9f68b542ec39c1b942901a284e1300335533a
eleanordm <eleanordm83@gmail.com>
2024-05-12 22:35:12 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   outputDir: "docs",
-  publicPath: process.env.NODE_ENV === "production" ? "/rick-and-morty/about" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/rick-and-morty/" : "/",
 })
