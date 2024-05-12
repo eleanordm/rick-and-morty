c8f824bd8d147f337af580815390264ec2240744
eleanordm <eleanordm83@gmail.com>
2024-05-12 22:00:15 +0100

    Public path added

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: "dist"
-
+  outputDir: "dist",
+  publicPath: process.env.NODE_ENV === "production" ? "/rick-and-morty/" : "/",
 })
