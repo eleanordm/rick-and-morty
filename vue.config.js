be055b0ece5460f734c007acd6e1be9fab8da0d5
eleanordm <eleanordm83@gmail.com>
2024-05-13 14:12:18 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: "dist",
+  outputDir: "docs",
   publicPath: process.env.NODE_ENV === "production" ? "/rick-and-morty/" : "/",
 })
