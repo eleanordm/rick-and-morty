99840021d6e69fd7cb1a90e6e0d383638cb798d2
eleanordm <eleanordm83@gmail.com>
2024-05-27 17:08:11 +0100

    Update to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+
     <body>
         <background-animation></background-animation>
         <div class="page-container relative min-h-screen">
@@ -41,7 +42,8 @@ export default {
 // Background colour for all pages
 body {
     background-image: linear-gradient(111.4deg, rgba(7, 7, 9, 1) 6.5%, rgb(13, 13, 38) 60%);
-    background-attachment: fixed
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center
 }
-
 </style>
