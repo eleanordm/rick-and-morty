539fa871b17434d0403441addc57b56baf691201
eleanordm <eleanordm83@gmail.com>
2024-05-27 16:37:32 +0100

    Background width and height added

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,9 @@ export default {
 // Background colour for all pages
 body {
     background-image: linear-gradient(111.4deg, rgba(7, 7, 9, 1) 6.5%, rgb(13, 13, 38) 60%);
+    height: 100vh;
+    width: 100vw
+
 }
 
 </style>
