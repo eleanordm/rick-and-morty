51738e0d7a4d51ef08a07be0dee6ffa354825ef1
eleanordm <eleanordm83@gmail.com>
2024-06-22 22:44:30 +0100

    Copy amend

--- src/components/WebsiteFooter.vue
+++ src/components/WebsiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-[#f69acc] w-full bottom-0 h-40 flex flex-col items-center justify-end pb-[8px] relative z-[1px]">
     <div class="text-md font-ubuntu text-white"><b>Eleanor Davis Mullen</b></div>
-    <div class="text-xs font-ubuntu text-white">Junior Software Devloper</div>
+    <div class="text-xs font-ubuntu text-white">Software Devloper</div>
     <div class="flex flex-row mt-2 font-ubuntu">
         <router-link to="/about" class="text-xs text-white cursor-pointer"><b>About</b></router-link>
         <router-link to="/search" class="text-xs ml-5 text-white cursor-pointer"><b>Search</b></router-link>
